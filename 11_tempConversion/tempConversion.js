8d886503ab14bd9a292878e0a3a96ba1ebbd11db
bgs <50104809+807376899@users.noreply.github.com>
2025-07-19 18:16:49 +0800

    finish 09 10 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let converTemp=(temp-32)*5/9;
+  if(Number.isInteger(converTemp)){
+    return converTemp;
+  }
+  return converTemp.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let converTemp=temp*9/5+32;
+  if(Number.isInteger(converTemp)){
+    return converTemp;
+  }
+  return converTemp.toFixed(1);
 };
 
 // Do not edit below this line
