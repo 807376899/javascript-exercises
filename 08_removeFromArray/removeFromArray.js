50e47f620904bce9a57f117d4f5b3f9c4d642598
bgs <50104809+807376899@users.noreply.github.com>
2025-07-19 17:36:02 +0800

    finish 06 07 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
 const removeFromArray = function() {
+    const args=Array.from(arguments);
+    len=arguments.length;
+    let arr=arguments[0];
+    for(i=1;i<len;i++){
+        for(j=0;j<arr.length;j++){
+            if(arguments[i]===arr[j]){
+                arr.splice(j,1);
+                j--;
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
